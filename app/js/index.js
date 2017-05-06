956031403b2f99d43d9879e95d7d99000eb0d45e
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-06 05:55:54 -0500

    Juego completo

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-  var container = $("div#Starfield").get(0);
+  var container = $('div#Starfield').get(0);
   var starfield = new Starfield();
   starfield.init(container);
   starfield.start();
-})
+
+  var canvas = $('canvas#GameCanvas').get(0);
+  canvas.width = 800;
+  canvas.height = 600;
+
+  var game = new Game();
+  game.init(canvas);
+  game.start();
+
+  $(window).keydown(function(e){
+    var keycode = e.which;
+    game.keydown(keycode);
+  });
+  $(window).keyup(function(e){
+    var keycode = e.which;
+    game.keyup(keycode);
+  });
+});
