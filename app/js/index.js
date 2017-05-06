68822305cb592700b8852dcb1a29b904d0554166
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-05 21:24:26 -0500

    Se termina el starfield

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  alert('hola mundo');
+  var container = $("div#Starfield").get(0);
+  var starfield = new Starfield();
+  starfield.init(container);
+  starfield.start();
 })
